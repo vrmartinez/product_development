31102cd12a8ea75127392bf4e0fa920b9f4b1d4f
NMedinaGalileo <natalie.medina@galileo.edu>
2025-11-16 20:56:34 -0600

    proyecto4 final

--- notebooks/operators.py
+++ notebooks/operators.py
@@ -13,3 +13,23 @@ class Mapper(BaseEstimator, TransformerMixin):
         for feature in self.variables:
             X[feature] = X[feature].map(self.mappings)
         return X
+
+
+class SimpleCategoricalImputer(BaseEstimator, TransformerMixin):
+    """
+    Imputador categórico sencillo:
+    - Rellena NaNs con un string (por defecto 'Missing').
+    """
+    def __init__(self, variables, fill_value="Missing"):
+        self.variables = variables
+        self.fill_value = fill_value
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for var in self.variables:
+            if var in X.columns:
+                X[var] = X[var].fillna(self.fill_value).astype(str)
+        return X
